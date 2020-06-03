60a3ad677a6ba3110cb80707dc1240affe1b3c2d
laijiaming <laijiaming@szboanda.net>
2020-06-03 17:42:08 +0800

    提交代码

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -6,7 +6,7 @@ def SonarScan(projectName,branchName){
     withSonarQubeEnv("sonar"){
         
         sh """ 
-           mvn sonar:sonar -Dsonar.host.url=http://192.168.3.158:30490 -Dsonar.login=$sonar-id -Dsonar.projectName=${project_name} -Dsonar.projectVersion=1.0 -Dsonar.sources=. -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=. -Dsonar.branch.name=${branchName}
+           mvn sonar:sonar -Dsonar.host.url=http://192.168.3.158:30490 -Dsonar.login=$sonar -Dsonar.projectName=${project_name} -Dsonar.projectVersion=1.0 -Dsonar.sources=. -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=. -Dsonar.branch.name=${branchName}
         """
     }
 
