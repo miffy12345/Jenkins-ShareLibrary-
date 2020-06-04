86a781e38c6fadbac2ff80885c19c27d20234e84
laijiaming <laijiaming@szboanda.net>
2020-06-04 16:57:36 +0800

    新增构建脚本

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -7,14 +7,14 @@ def compile(){
         """
 }
 
-#构建打包
+//构建打包
 def package(){
         sh """
            mvn clean package -Dmaven.test.skip=true
         """
 }
 
-#手动打包
+//手动打包
 def Artifacts(){
         sh '''cd ${WORKSPACE}/src/main/webapp
           jar -cvf ${project_name}.war ./*'''
