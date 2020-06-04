38e70863dbcf8844cbb2d4646a03ef6247d6e4e7
laijiaming <laijiaming@szboanda.net>
2020-06-04 16:35:35 +0800

    更新拉取代码脚本

--- src/org/devops/checkscm.groovy
+++ src/org/devops/checkscm.groovy
@@ -1,11 +1,11 @@
 package org.devops
 
-#拉取git代码
+//拉取git代码
 def checkgit(giturl,gitbranch){
         git(url: '${giturl}', credentialsId: 'gitlab-id', branch: '${git}', changelog: true, poll: false)
 }
 
-#拉取svn代码
+//拉取svn代码
 def checksvn(svnurl){
          checkout(scm: [$class: 'SubversionSCM', locations: [[cancelProcessOnExternalsFail: true,  credentialsId: 'svn-id', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: '${svnurl}']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']], poll: false)
-}
+}m
