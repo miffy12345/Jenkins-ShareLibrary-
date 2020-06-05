de2c99d1c331eabf2406b9a80c2b563c0c74235a
miffy12345 <65323759+miffy12345@users.noreply.github.com>
2020-06-05 11:43:58 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 //构建
 def compile(){
-     sh """
+        sh """
            mvn clean compile
         """
 }
@@ -16,11 +16,10 @@ def buildpackage(){
 //构建镜像
 def docker_images(libary_name,images_name,tag_name,giturl_branch){
         sh """
-           ENV_WEBAPP=$(echo ${images_name}_${tag_name} | tr '[a-z]' '[A-Z]')
            cp  ${WORKSPACE}/src/main/docker/Dockerfile ${WORKSPACE}/target/
            cp ${WORKSPACE}/script/build_pom.sh ${WORKSPACE}/target/
            cd ${WORKSPACE}/target/
-           sh build_pom.sh ${libary_name} ${images_name} ${tag_name} ${ENV_WEBAPP} ${giturl_branch}
+           sh build_pom.sh ${libary_name} ${images_name} ${tag_name} ${images_name}_${tag_name} ${giturl_branch}
         """
 }
 //手动打包
