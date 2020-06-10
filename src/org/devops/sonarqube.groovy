c52314b6ea53eb654e18cdcc561e46a40f42699c
miffy12345 <65323759+miffy12345@users.noreply.github.com>
2020-06-10 11:52:32 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,12 +1,12 @@
 package org.devops
 
-def SonarScan(sonarname){
+def SonarScan(sonar_name){
     
     withCredentials([string(credentialsId : 'sonar-id' ,variable : 'sonar' ,)]){
     withSonarQubeEnv("sonar"){
         
         sh """ 
-           mvn sonar:sonar -Dsonar.host.url=http://192.168.3.158:30490 -Dsonar.login=$sonar -Dsonar.projectName=${sonarname} -Dsonar.projectVersion=1.0 -Dsonar.sources=. -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=.
+           mvn sonar:sonar -Dsonar.host.url=http://192.168.3.158:30490 -Dsonar.login=$sonar -Dsonar.projectName=${sonar_name} -Dsonar.projectVersion=1.0 -Dsonar.sources=. -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=.
         """
     }
 
