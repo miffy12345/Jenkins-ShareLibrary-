e9642d540502a3a0248dda2c1ea3f259dabff335
miffy12345 <65323759+miffy12345@users.noreply.github.com>
2020-06-10 10:46:32 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -14,14 +14,14 @@ def buildpackage(){
         """
 }
 //构建镜像
-def docker_images(libary_name,images_name,tag_name,giturl_branch){
+def docker_images(libary_name,images_name,tag_name){
         sh '''
            images=$(echo ${images_name} | tr '[A-Z]' '[a-z]')
            tag=$(echo ${tag_name} | tr '[A-Z]' '[a-z]')
            cp  ${WORKSPACE}/src/main/docker/Dockerfile ${WORKSPACE}/target/
            cp ${WORKSPACE}/script/docker/build_pom.sh ${WORKSPACE}/target/
            cd ${WORKSPACE}/target/
-           sh build_pom.sh ${libary_name} ${images} ${tag} ${images_name}-${tag_name} ${giturl_branch}
+           sh build_pom.sh ${libary_name} ${images} ${tag} ${images_name}-${tag_name}
         '''
 }
 //手动打包
