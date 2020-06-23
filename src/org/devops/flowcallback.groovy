524fe7ef430c9e402c4eae786ab1c3968a638553
miffy12345 <65323759+miffy12345@users.noreply.github.com>
2020-06-23 09:45:55 +0800

    Update flowcallback.groovy

--- src/org/devops/flowcallback.groovy
+++ src/org/devops/flowcallback.groovy
@@ -1,1 +1,15 @@
+package org.devops
+
+//回调节点日志
+def flowcallback(project_name,build_id,step_name,flow_name){
+        sh """
+           printf '{
+             "project_name":"project-EK2kzr8N08jW/job/maven-test9",
+               "build_id":"41",
+               "step_name":"build",
+               "flow_name":"测试流水线"
+           }'| http  --follow --timeout 3600 POST 2.2.2.104:32215/gitlab/api/integration/flowlog/flowcallback \
+           Content-Type:'application/json'
+        """
+}
 
