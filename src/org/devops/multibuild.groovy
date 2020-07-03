61dc7dfcf60721dc153c9b0bd1b78d45f0cefbf0
miffy12345 <65323759+miffy12345@users.noreply.github.com>
2020-07-03 12:10:18 +0800

    Update multibuild.groovy

--- src/org/devops/multibuild.groovy
+++ src/org/devops/multibuild.groovy
@@ -3,14 +3,14 @@ package org.devops
 //构建打包（包含多种打包类型）
 def multibuild(system_type){
         
-        if("${buildType}" == "Maven"){
+    if("${system_type}" == "Maven"){
            sh '''
                mvn package -Dmaven.test.skip=true
             '''
-        }
-        else{
-            sh '''
+    }
+    else{
+        sh '''
                echo"当前选择的构建类型为其他"
-            '''
-        }
+           '''
+    }
 }
